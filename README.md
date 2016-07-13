2fefbb5e8514a67301ccce66c7599e4eaf06e93d
romathonat <romain.mathonat@gmail.com>
2016-07-13 09:24:58 +0200

    add demo.gif

--- README.md
+++
@@ -1,5 +0,0 @@
-
-# markdown-react-demo
-
-This project show you how to use .md (markdown) files into a react project.
-
--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Reading from './components/Reading.js';
 
 import markdown1 from 'raw!./markdown/markdown1.md';
 import redux_intro from 'raw!./markdown/redux_intro.md';
+import markdown_cheatsheet from 'raw!./markdown/Markdown-Cheatsheet.md';
 
 var markdownFiles = [
   {'title': 'markdown1.md', 'file': markdown1},
-  {'title': 'redux_intro.md', 'file': redux_intro}];
+  {'title': 'redux_intro.md', 'file': redux_intro},
+  {'title': 'markdown_cheatsheet.md', 'file': markdown_cheatsheet}];
 
 export default class App extends Component {
   constructor() {
